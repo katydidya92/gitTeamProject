a8a9422475a4a50751cd8c2f4cbf147a34798e55
katydidya92 <sweets1327@gmail.com>
2021-03-19 19:52:53 +0900

    add lib modify backup

--- src/com/tp/backup/DAO.java
+++ src/com/tp/backup/DAO.java
@@ -3,26 +3,39 @@ package com.tp.backup;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class DAO {
-	private static DAO dao;
-	private Connection con;
+	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	String sql ="";
 	
-	private DAO(){}
+	private Connection getCon() throws Exception{
+		Context initCTX = new InitialContext();
+		DataSource ds 
+		   = (DataSource)initCTX.lookup("java:comp/env/jdbc/tpDB");
+		con = ds.getConnection();
+		
+		System.out.println("DAO : 디비 연결 성공!");
+		
+		return con;
+	}
 	
-	public static DAO getInstance() {
-		if (dao == null) {
-			dao = new DAO();
+	// 자원해제 
+	public void closeDB(){
+		try {
+			if(rs != null){ rs.close(); }
+			if(pstmt != null){ pstmt.close(); }
+			if(con != null){ con.close(); }
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		return dao;
 	}
-	
-	public void setConnection(Connection con) {
-		this.con = con;
-	}
-	
 	
 	
 }
